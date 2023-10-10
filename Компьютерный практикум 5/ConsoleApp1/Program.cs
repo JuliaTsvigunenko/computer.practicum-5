05aeb7fc6b4e4610ae880cbf441050072b8c13b2
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2023-10-10 22:12:37 +0300

    Add files via upload

---
+++ Компьютерный практикум 5/ConsoleApp1/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Title="Программа №1(Цвигуненко)";
+            //int year;
+            //Console.WriteLine("Введите год:");
+            //year = Convert.ToInt32(Console.ReadLine());
+            //if (year % 4 == 0)
+            //{
+            //    Console.WriteLine($"{year}год - високосный");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{year}год - невисокосный");
+            //}
+            //Console.ReadKey();
+
+            //Console.Title = "Программа №2(Цвигуненко)";
+            //int price;
+            //Console.WriteLine("введите цену ноутбука:");
+            //price = Convert.ToInt32(Console.ReadLine());
+            //if(price>=20000 & price<=30000)
+            //    {
+            //    Console.WriteLine($"price: входит в диапозон!");
+            //    }
+            //else
+            //{
+            //    Console.WriteLine($"price: не входит в диапозон!");
+            //}
+            //Console.ReadKey();
+
+            //Console.Title = "Программа №3(Цвигуненко)";
+            //byte score;
+            //Console.WriteLine("Введите балл учащегося:");
+            //score = Convert.ToByte(Console.ReadLine());
+            //if(score >= 0 && score <= 59)
+            //{
+            //    Console.WriteLine($"Неудовлетворительно");
+            //}
+            //else if(score >=60 && score <=74)
+            //{
+            //    Console.WriteLine($"Удовлетворительно");
+            //}
+            //else if (score >= 75 && score <= 89)
+            //{
+            //    Console.WriteLine($"Хорошо");
+            //}
+            //else if (score >= 90 && score <= 100)
+            //{
+            //    Console.WriteLine($"Отлично");
+            //}
+            //Console.ReadKey();
+
+            //Console.Title = "Программа №4(Цвигуненко)";
+            //string svetofor;
+            //Console.Write("Введите состояние светофора:");
+            //svetofor = Convert.ToString(Console.ReadLine());
+            //switch(svetofor)
+            //{
+            //    case "1":Console.WriteLine("Красный"); break;
+            //    case "2": Console.WriteLine("Желтый"); break;
+            //    case "3": Console.WriteLine("Зеленый"); break;
+            //    default:Console.WriteLine("ошибка");break;
+            //}
+            //Console.ReadKey();
+
+            //Console.Title = "Программа №5(Цвигуненко)";
+            //int number;
+            //Random run = new Random();
+            //number = run.Next(-5, 5);
+            //Console.WriteLine(number);
+            //if (number % 2 == 0)
+            //{
+            //    Console.WriteLine($"Четное");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Нечетное");
+            //}
+            //if (number > 0)
+            //{
+            //    Console.WriteLine("Число положительное");
+            //}
+            //else if(number<0)
+            //{
+            //    Console.WriteLine("Число отрицательное");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Число равное нулю");
+            //}
+            //Console.ReadKey();
+
+
+            //Console.Title = "Программа №6(Цвигуненко)";
+            //string day;
+            //Console.Write("Введите порядковый день недели:");
+            //day = Convert.ToString(Console.ReadLine());
+            //switch (day)
+            //{
+            //    case "1": Console.WriteLine("Понедельник"); break;
+            //    case "2": Console.WriteLine("Вторник"); break;
+            //    case "3": Console.WriteLine("Среда"); break;
+            //    case "4": Console.WriteLine("Четверг"); break;
+            //    case "5": Console.WriteLine("Пятница"); break;
+            //    case "6": Console.WriteLine("Суббота"); break;
+            //    case "7": Console.WriteLine("Воскресенье"); break;
+            //    default: Console.WriteLine("ошибка"); break;
+            //}
+            //Console.ReadKey();
+
+
+            //Console.Title = "Программа №9(Цвигуненко)";
+            //Console.Write("Введите свой возраст:");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.Write($"Ваша возрастная группа:");
+            //if (age < 0)
+            //{
+            //    Console.WriteLine("Недействительный возраст");
+            //}
+            //else if (age >= 0 && age <= 2)
+            //{
+            //    Console.WriteLine("Младенец");
+            //}
+            //else if (age >= 3 && age <= 12)
+            //{
+            //    Console.WriteLine("Ребенок");
+            //}
+            //else if (age >= 13 && age <= 19)
+            //{
+            //    Console.WriteLine("Подросток");
+            //}
+            //else if (age >= 20 && age <= 44)
+            //{
+            //    Console.WriteLine("Молодой возраст");
+            //}
+            //else if (age >= 45 && age <= 59)
+            //{
+            //    Console.WriteLine("Средний возраст");
+            //}
+            //else if (age >= 60 && age <= 74)
+            //{
+            //    Console.WriteLine("Рання старость");
+            //}
+            //else if (age >= 75 && age <= 100)
+            //{
+            //    Console.WriteLine("Старость");
+            //}
+
+            //Console.ReadKey();
+        }
+    }
+}
